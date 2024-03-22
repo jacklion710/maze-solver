23a81affecc61cb39b3014a3493ff712e2875346
jacklion710 <jack.lion710@gmail.com>
2024-03-22 16:47:48 -0400

    Class syntax fix

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
-# cell.py
 from graphics import Line, Point
 
-class Cell():
+
+class Cell:
     def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
@@ -13,7 +13,7 @@ class Cell():
         self._y2 = None
         self._win = win
 
-    def draw(self, x1, x2, y1, y2):
+    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
